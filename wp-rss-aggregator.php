37ad64c2e323c83105a863d58e993dc92f10dd6d
jeangalea <jeangalea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-06 17:14:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-rss-aggregator/trunk@485777 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -102,6 +102,7 @@
         echo "<script type='text/javascript'>jQuery(document).ready(function(){ jQuery('a.colorbox').colorbox({iframe:true, width:'80%', height:'80%'});});</script>";
     }
 
+
     /**
      * Include CSS in plugin page header
      */ 
